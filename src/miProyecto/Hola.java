81a8b65c7485eb56446904a547877022d4e74979
Etna2886 <etna2886gmail.com>
2025-11-03 11:56:20 +0100

    Primer commit: añado hola.java

---
+++ src/miProyecto/Hola.java
@@ -0,0 +1,12 @@
+package miProyecto;
+
+public class Hola {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hola mi proyecto");
+		
+	}
+
+}
